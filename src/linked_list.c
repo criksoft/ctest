8e9e2aacca86c5265c7bc6440a5ee8d30a3915d0
dazga <dazga@criksoft.com>
2026-04-22 19:30:19 -0700

    Linked list exercise

---
+++ src/linked_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Append Value
+// Prepend Value
+// Delete a Value
+// Delete an Index
+// Get Size
+// Print the list
+// Modify a value on an index
+
+int main(void)
+{
+
+}
