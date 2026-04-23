f83bebc9409034c8317d47ab8ca5f163f2ad9287
dazga <dazga@criksoft.com>
2026-04-22 19:22:11 -0700

    Add more polymorphism examples and container_of macro

--- src/polymorphism.c
+++ src/polymorphism.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define container_of(ptr, type, member) ((type *)((char *)(ptr) - offsetof(type, member)))
+
 struct speaker {
     struct speaker_ops *ops;
 };
@@ -24,17 +26,31 @@ struct dog {
     char name[20];
 };
 
+struct channel_dog {
+    struct speaker speaker;
+    struct eater eater;
+    char name[20];
+};
+
 void dog_speak(struct speaker *self)
 {
-    struct dog *dog = (struct dog *)((char *)self - offsetof(struct dog, speaker));
+    struct dog *dog = container_of(self, struct dog, speaker);
     printf("My name is %s and I'm speaking!\n", dog->name);
+}
+
+void channel_speak(struct speaker *self)
+{
+    struct dog *dog = container_of(self, struct dog, speaker);
+    printf("wooooof!!\n");
 }
 
 void dog_eat(struct eater *self)
 {
-    struct dog *dog = (struct dog *)((char *)self - offsetof(struct dog, eater));
+    struct dog *dog = container_of(self, struct dog, eater);
     printf("My name is %s and I'm eating!\n", dog->name);
 }
+
+struct speaker_ops channel_speaker_ops = {.speak = channel_speak};
 
 struct speaker_ops dog_speaker_ops = {.speak = dog_speak};
 
@@ -44,6 +60,17 @@ struct dog new_dog(const char *name)
 {
     struct dog dog = {
         .speaker.ops = &dog_speaker_ops,
+        .eater.ops = &dog_eater_ops,
+    };
+    strncpy(dog.name, name, sizeof(dog.name) - 1);
+    dog.name[sizeof(dog.name) - 1] = '\0';
+    return dog;
+}
+
+struct channel_dog new_channel(const char *name)
+{
+    struct channel_dog dog = {
+        .speaker.ops = &channel_speaker_ops,
         .eater.ops = &dog_eater_ops,
     };
     strncpy(dog.name, name, sizeof(dog.name) - 1);
@@ -64,8 +91,11 @@ void get_the_eater_to_eat(struct eater *eater)
 int main()
 {
     struct dog dog = new_dog("Buddy");
+    struct channel_dog channel = new_channel("Channel");
 
+    get_the_speaker_to_speak(&channel.speaker);
     get_the_speaker_to_speak(&dog.speaker);
+
     get_the_eater_to_eat(&dog.eater);
 
     dog.speaker.ops->speak(&dog.speaker);
